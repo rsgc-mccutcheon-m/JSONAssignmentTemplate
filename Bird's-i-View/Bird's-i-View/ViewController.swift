a317659c476ae765e37a782b93ee1188375b0ad1
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-05-18 22:10:28 -0400

    This is my initial commit
    This initial commit. I did some work modifying mr gordon’s example code
    before this commit, so to summarize what I’ve done: I’ve moved over the
    template functions I need, I’ve created two basic handlers for the
    CLMotionmanager delegate, I’ve set up a url builder code snippet that
    takes input from gps coordinate variables, and finally I set up some
    general purpose storage variables.

---
+++ Bird's-i-View/Bird's-i-View/ViewController.swift
@@ -0,0 +1,216 @@
+//
+//  ViewController.swift
+//  Bird's-i-View
+//
+//  Created by Mark on 2016-05-16.
+//  Copyright © 2016 Clutch Industries. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreLocation
+
+
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+   
+    
+    let manager : CLLocationManager = CLLocationManager()
+    
+    var lat : String = ""
+    var long : String = ""
+    var latDouble : Double = 0.0
+    var longDouble : Double = 0.0
+    
+    
+    @IBOutlet var GPS : UILabel!
+    @IBOutlet var date : UILabel!
+
+    @IBOutlet weak var Image: UIImageView!
+
+    
+    // If data is successfully retrieved from the server, we can parse it here
+    func parseMyJSON(theData : NSData) {
+        
+        // Print the provided data
+        print("")
+        print("====== the data provided to parseMyJSON is as follows ======")
+        print(theData)
+        
+        // De-serializing JSON can throw errors, so should be inside a do-catch structure
+        do {
+            
+            // Do the initial de-serialization
+            
+            let json = try NSJSONSerialization.JSONObjectWithData(theData, options: NSJSONReadingOptions.AllowFragments) as! AnyObject
+            
+            // Print retrieved JSON
+            print("")
+            print("====== the retrieved JSON is as follows ======")
+            print(json)
+            
+            // Now we can parse this...
+            print("")
+            print("Now, add your parsing code here...")
+            
+            // Now we can update the UI
+            // (must be done asynchronously)
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                
+            }
+            
+        } catch let error as NSError {
+            print ("Failed to load: \(error.localizedDescription)")
+        }
+        
+        
+    }
+    
+    // Set up and begin an asynchronous request for JSON data
+    func getMyJSON() {
+        
+        // Define a completion handler
+        // The completion handler is what gets called when this **asynchronous** network request is completed.
+        //https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&cloud_score=False&api_key=HP7NhvAtAFV4AiPhn1VViToDCcGCco2Qb8kJGcjA
+        //we need to first build the url!
+        
+        var urlString : String = "https://api.nasa.gov/planetary/earth/imagery?"
+        
+        urlString += ("lon=" + long + "&" + "lat=" + long + "&cloud_score=False&api_key=HP7NhvAtAFV4AiPhn1VViToDCcGCco2Qb8kJGcjA")
+        
+        
+        
+        
+        // This is where we'd process the JSON retrieved
+        let myCompletionHandler : (NSData?, NSURLResponse?, NSError?) -> Void = {
+            
+            (data, response, error) in
+            
+            // This is the code run when the network request completes
+            // When the request completes:
+            //
+            // data - contains the data from the request
+            // response - contains the HTTP response code(s)
+            // error - contains any error messages, if applicable
+            
+            print("")
+            print("====== data from the request follows ======")
+            print(data)
+            print("")
+            print("====== response codes from the request follows ======")
+            print(response)
+            print("")
+            print("====== errors from the request follows ======")
+            print(error)
+            
+            // Cast the NSURLResponse object into an NSHTTPURLResponse objecct
+            if let r = response as? NSHTTPURLResponse {
+                
+                // If the request was successful, parse the given data
+                if r.statusCode == 200 {
+                    
+                    if let d = data {
+                        
+                        // Parse the retrieved data
+                        self.parseMyJSON(d)
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        // Define a URL to retrieve a JSON file from
+        let address : String = urlString
+        
+        // Try to make a URL request object
+        if let url = NSURL(string: address) {
+            
+            // We have an valid URL to work with
+            print(url)
+            
+            // Now we create a URL request object
+            let urlRequest = NSURLRequest(URL: url)
+            
+            // Now we need to create an NSURLSession object to send the request to the server
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            // Now we create the data task and specify the completion handler
+            let task = session.dataTaskWithRequest(urlRequest, completionHandler: myCompletionHandler)
+            
+            // Finally, we tell the task to start (despite the fact that the method is named "resume")
+            task.resume()
+            
+        } else {
+            
+            // The NSURL object could not be created
+            print("Error: Cannot create the NSURL object.")
+            
+        }
+        
+    }
+    
+    // This is the method that will run as soon as the view controller is created
+    override func viewDidLoad() {
+        
+        // Sub-classes of UIViewController must invoke the superclass method viewDidLoad in their
+        // own version of viewDidLoad()
+        super.viewDidLoad()
+        
+        
+        
+      
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // Required method for CLLocationManagerDelegate
+    // This method runs when the location of the user has been updated.
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // We now have the user's location, so stop finding their location.
+        // (Looking for current location is a battery drain)
+        self.manager.stopUpdatingLocation()
+        
+        // Set the most recent location found
+        let latestLocation = locations.last
+        
+        // Format the current location as strings with four decimal places of accuracy
+        lat = String(format: "%.4f", latestLocation!.coordinate.latitude)
+        long = String(format: "%.4f", latestLocation!.coordinate.longitude)
+        
+        // Save the current location as a Double
+        latDouble = Double(latestLocation!.coordinate.latitude)
+        longDouble = Double(latestLocation!.coordinate.longitude)
+        
+        // Report the location
+        print("Location obtained at startup...")
+        print("Latitude: \(latDouble)")
+        print("Longitude: \(longDouble)")
+        
+        // Now actually retrieve the cooling centre data
+        getMyJSON()
+    }
+    
+    // Required method for CLLocationManagerDelegate
+    // This method will be run when there is an error determing the user's location
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        // Report the error
+        print("didFailWithError \(error)")
+        
+    }
+
+
+}
+
